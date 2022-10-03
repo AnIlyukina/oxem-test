5def8bd6783aade5f58a4cc221b1bca46c14f9c7
Ilyukina <anyailyukina@yandex.ru>
2022-10-03 22:38:57 +0800

    Создание проекта, начальная структура папок проекта

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CalculationCarLising />
+  </div>
+</template>
+
+<script>
+import CalculationCarLising from "./components/CalculationCarLising";
+export default {
+  name: "App",
+
+  components: {
+    CalculationCarLising,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/scss/app.scss";
+</style>
---
+++ src/components/CalculationCarLising.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: "CalculationCarLising"
+  
+};
+</script>
+
+<style></style>
