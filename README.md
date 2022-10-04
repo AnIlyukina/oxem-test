e99bc7803a45f1500351f9fd87af41175ec87b40
Ilyukina <anyailyukina@yandex.ru>
2022-10-04 18:38:33 +0800

    Вынесла константы для инициализации калькулятора и методы для проверки мин и макс значений в миксин, деплой проекта

--- README.md
+++ README.md
@@ -1,24 +1,12 @@
 # oxem-test
 
-## Project setup
+## Запуcк проекта:
 ```
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
 npm run serve
 ```
 
-### Compiles and minifies for production
+### Ссылка на приложение
 ```
-npm run build
+https://oxen-test.web.app
 ```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/CalculationCarLising.vue
+++ src/components/CalculationCarLising.vue
@@ -150,21 +150,12 @@
 <script>
 
 import { sendCalcData } from "../fetch";
+import { defaultParamsCalculator } from "../mixins/defaultParamsCalculator";
 
 export default {
   name: "CalculationCarLising",
   data() {
     return {
-      defaultPriceCar: 3300000,
-      defaultTermLease: 60,
-      defaultInitialPercent: 13,
-      percentRate: 3.5,
-      minPriceCar: 1000000,
-      maxPriceCar: 6000000,
-      minInitialPercent: 10,
-      maxInitialPercent: 60,
-      minTermLease: 1,
-      maxTermLease: 60,
       priceCar: 0,
       priceCarInput: '',
       initialPay: 0,
@@ -177,6 +168,7 @@ export default {
       successMessage: ""
     };
   },
+  mixins: [defaultParamsCalculator],
   computed: {
     disabledButton() {
       return this.sumLeasing === 0 || this.monthPay === 0;
@@ -262,34 +254,6 @@ export default {
       this.initialPercent = this.checkInitialPaymentValue(value)
       this.calcInitialPay()
     },
-    checkPriceCarValue (value) {
-      if (value < this.minPriceCar) {
-          return  this.minPriceCar;
-      } else if (value > this.maxPriceCar) {
-          return this.maxPriceCar;
-      }  else {
-          return value
-      }
-    },
-    checkTermLeaseValue (value) {
-      if (value < this.minTermLease) {
-          return this.minTermLease;
-      } else if (value > this.maxTermLease) {
-          return this.maxTermLease;
-      } else {
-          return value
-      }
-    },
-    checkInitialPaymentValue (value) {
-        if (this.minInitialPercent > value) {
-            return this.minInitialPercent;
-        } else if (value > this.maxInitialPercent) {
-            return this.maxInitialPercent;
-        } else {
-            return value
-        }
-    },
-
     setActiveClass(e) {
       if (e.target.id === 'initialPay') {
         this.initialPay = this.initialPercent;
@@ -298,7 +262,7 @@ export default {
     },
 
     deleteActiveClass(e) {
-      if (e.target.id === 'initialPay' &&  this.initialPay == this.initialPercent) {
+      if (e.target.id === 'initialPay' &&  this.initialPay === this.initialPercent) {
         this.calcInitialPay()
       }
       e.target.closest(".calculator__input-block").classList.remove("active");
